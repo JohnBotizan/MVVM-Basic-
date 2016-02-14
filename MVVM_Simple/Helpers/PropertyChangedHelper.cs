a59fd24de9da67905af4d017f5efcec6a54e050a
JohnBotizan <botizanionutciprian@gmail.com>
2016-02-14 08:40:59 +0000

    Added PropertyChanged  for Binding in MVVM

---
+++ MVVM_Simple/Helpers/PropertyChangedHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace MVVM_Simple.Helpers
+{
+   public  class PropertyChangedHelper : INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string parameterName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(parameterName));
+        }
+   
+   }
+}
